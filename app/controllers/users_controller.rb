7414054779745acfd9ce1b3a1c849ac7a8152188
Sarah Schaller <sarcsch@aol.com>
2018-02-24 22:58:47 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params).save
+    @user = User.new(user_params)
+    if @user.save 
+      current_user = @user.id 
+      redirect_to users_index_path 
+    else 
+      redirect_to new_user_path
   end
 
   def destroy
