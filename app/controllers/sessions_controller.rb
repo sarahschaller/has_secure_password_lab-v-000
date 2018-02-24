1775f97adf59e9982d6b6c21df9ca6d7656762d2
Sarah Schaller <sarcsch@aol.com>
2018-02-24 23:00:16 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:user][:name])
-    if @user 
+    if @user
       return head(:forbidden) unless @user.authenticate(params[:user][:password])
       current_user = @user.id
-      redirect_to users_index_path 
-    else 
+      redirect_to users_index_path
+    else
       redirect_to '/sessions/new'
     end
   end
